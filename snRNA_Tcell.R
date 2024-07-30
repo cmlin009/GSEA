5b4d4b7e78a19fc9fb766a85cb9c054c1394b847
cmlin009 <cmlin009@gmail.com>
2024-07-31 04:31:24 +0800

    Update snRNA_Tcell.R

--- snRNA_Tcell.R
+++ snRNA_Tcell.R
@@ -29,7 +29,7 @@ GSEA_input5_PN = sort(GSEA_input5_PN, decreasing = TRUE)
 GSEA_KEGG5_PN <- gseKEGG(GSEA_input5_PN, organism = KEGG_database, pvalueCutoff = 1)
 write.csv(GSEA_KEGG5_PN, 'CHRNA5_snRNA_GSEA_KEGG_Tcell_positive_negative.csv')
 
-GSEA_GO5_PN <- gseGO(GSEA_input5_PN, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_GO5_PN <- gseGO(GSEA_input5_PN, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1)
 write.csv(GSEA_GO5_PN, 'CHRNA5_snRNA_GSEA_GO_Tcell_positive_negative.csv')
 
 gseaplot2(GSEA_KEGG5_PN,265) #PD-L1 expression and PD-1 checkpoint pathway in cancer
@@ -47,10 +47,10 @@ GSEA_input5_HL <- info5_HL$Difference
 names(GSEA_input5_HL) = info5_HL$ENTREZID
 GSEA_input5_HL = sort(GSEA_input5_HL, decreasing = TRUE)
 
-GSEA_KEGG5_HL <- gseKEGG(GSEA_input5_HL, organism = KEGG_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_KEGG5_HL <- gseKEGG(GSEA_input5_HL, organism = KEGG_database, pvalueCutoff = 1)
 write.csv(GSEA_KEGG5_HL, 'CHRNA5_snRNA_GSEA_KEGG_Tcell_high_low.csv')
 
-GSEA_GO5_HL <- gseGO(GSEA_input5_HL, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_GO5_HL <- gseGO(GSEA_input5_HL, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1)
 write.csv(GSEA_GO5_HL, 'CHRNA5_snRNA_GSEA_GO_Tcell_high_low.csv')
 
 gseaplot2(GSEA_KEGG5_HL,150) #PD-L1 expression and PD-1 checkpoint pathway in cancer
@@ -61,17 +61,17 @@ gseaplot2(GSEA_GO5_HL,77) #T cell activation
 CHRNA7_PN <- read.delim("~/OneDrive/文件/CHRNA/snRNAseq Puram 2017/GSEA_T cell/Multiple unpaired t tests of CHRNA7 T cell mRNA DEG positive negative 刪除全零.txt")
 CHRNA7_PN <- rename(CHRNA7_PN, "X" = "Gene")
 gene7_PN <- bitr(CHRNA7_PN$Gene,fromType = 'SYMBOL',toType = 'ENTREZID',OrgDb = GO_database)
-gene7_PN <- rename (gene7_PN, "SYMBOL" = 'Gene') #不知為何變成舊的名稱在前面了
+gene7_PN <- rename (gene7_PN, "SYMBOL" = 'Gene')
 info7_PN <- left_join(CHRNA7_PN, gene7_PN, by = "Gene")
 
 GSEA_input7_PN <- info7_PN$Difference
 names(GSEA_input7_PN) = info7_PN$ENTREZID
 GSEA_input7_PN = sort(GSEA_input7_PN, decreasing = TRUE)
 
-GSEA_KEGG7_PN <- gseKEGG(GSEA_input7_PN, organism = KEGG_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_KEGG7_PN <- gseKEGG(GSEA_input7_PN, organism = KEGG_database, pvalueCutoff = 1)
 write.csv(GSEA_KEGG7_PN, 'CHRNA7_snRNA_GSEA_KEGG_Tcell_positive_negative.csv')
 
-GSEA_GO7_PN <- gseGO(GSEA_input7_PN, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_GO7_PN <- gseGO(GSEA_input7_PN, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1)
 write.csv(GSEA_GO7_PN, 'CHRNA7_snRNA_GSEA_GO_Tcell_positive_negative.csv')
 
 gseaplot2(GSEA_KEGG7_PN,169) #PD-L1 expression and PD-1 checkpoint pathway in cancer
@@ -89,10 +89,10 @@ GSEA_input7_HL <- info7_HL$Difference
 names(GSEA_input7_HL) = info7_HL$ENTREZID
 GSEA_input7_HL = sort(GSEA_input7_HL, decreasing = TRUE)
 
-GSEA_KEGG7_HL <- gseKEGG(GSEA_input7_HL, organism = KEGG_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_KEGG7_HL <- gseKEGG(GSEA_input7_HL, organism = KEGG_database, pvalueCutoff = 1)
 write.csv(GSEA_KEGG7_HL, 'CHRNA7_snRNA_GSEA_KEGG_Tcell_high_low.csv')
 
-GSEA_GO7_HL <- gseGO(GSEA_input7_HL, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1) #GSEA富集分析
+GSEA_GO7_HL <- gseGO(GSEA_input7_HL, ont = "ALL", OrgDb = GO_database, pvalueCutoff = 1)
 write.csv(GSEA_GO7_HL, 'CHRNA7_snRNA_GSEA_GO_Tcell_high_low.csv')
 
 gseaplot2(GSEA_KEGG7_HL,60) #PD-L1 expression and PD-1 checkpoint pathway in cancer
